523ade8b4b3448edba5012e2688d8c68f2891681
Manish24076 <971Shakshi@gmail.com>
2026-02-17 19:44:35 +0530

    Added Operators Java program

---
+++ Operators/Arthmatic.java
@@ -0,0 +1,23 @@
+package Operators;
+
+public class Arthmatic {
+    public static void main(String[] args) {
+        int a =10;
+        int b = 11;
+
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        int mod = a %  b ;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Sub: " + sub);
+        System.out.println("Mul: " + mul);  
+        System.out.println("Div: " + div);
+        System.out.println("Mod: " + mod);
+
+
+       
+    }
+}
