ef3301ead14858b10adaa1368dbae9f8ecdaf0d8
Manish24076 <971Shakshi@gmail.com>
2026-02-17 21:09:27 +0530

    Test value change of a

---
+++ ARRAY/Main.java
@@ -0,0 +1,7 @@
+package ARRAY;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java successfully running in VS Code 🚀");
+    }
+}
--- Operators/Arthmatic.java
+++ Operators/Arthmatic.java
@@ -2,7 +2,7 @@ package Operators;
 
 public class Arthmatic {
     public static void main(String[] args) {
-        int a =10;
+        int a =100;
         int b = 11;
 
         int sum = a + b;
